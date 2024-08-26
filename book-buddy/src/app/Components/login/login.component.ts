4246866149fab89968acaed51ae0d08a70229e7b
Neil Quirk <73691309+Neil-Quirk@users.noreply.github.com>
2024-08-26 10:31:00 +0200

    Frontend setup
    Create project and load dependencies for the frontend framework
    Create the login component and mock-up of the login page

---
+++ book-buddy/src/app/Components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit{
+loginForm!: FormGroup;
+
+constructor() {}
+
+ngOnInit(): void {
+  this.loginForm = this.createFormGroup();
+}
+createFormGroup(): FormGroup {
+  return new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(7)]),
+  });
+}
+
+}
